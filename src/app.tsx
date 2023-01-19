cee32f1744474c6a2157e49f80a60851915b4a53
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2023-01-19 15:49:47 -0300

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,15 @@
 //import { Habit } from './components/Habit'
 import "./styles/global.css"
+import logoImage from './assets/logo.svg'
+
 
 export function App() {
   return (
-    <>
-    
-    </>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <img src={logoImage} alt="" />
+      </div>
+    </div>
   )
 }
 
