8837b48aad67f38176070b9c70b0cdea52d583b2
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2023-01-19 15:51:58 -0300

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -7,7 +7,7 @@ export function App() {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
       <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
-        <img src={logoImage} alt="" />
+        <img src={logoImage} alt="Habits" />
       </div>
     </div>
   )
