ac77a69a8add6ac244077a19191f491e0d3ca42a
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2023-01-19 16:10:20 -0300

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,13 @@
 //import { Habit } from './components/Habit'
+import { Header } from "./components/Header"
 import "./styles/global.css"
-import logoImage from './assets/logo.svg'
-import {Plus} from 'phosphor-react'
 
 export function App() {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
       <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        
+      <Header/>
       </div>
     </div>
   )
