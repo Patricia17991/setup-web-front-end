632f15d4e4f9afb5a92ab187ce04c36bf0ade8f6
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2023-01-19 16:08:25 -0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,19 @@
-    <div className="w-full max-w-3xl mx-auto flex items-center justify-between">
-          <img src={logoImage} alt="Habits" />
+import { Plus } from "phosphor-react";
+import logoImage from './assets/logo.svg'
 
-          <button
-           type="button"
-           className="border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300"
-           >
-            <Plus size={20} className="text-violet-500"/>
-            Novo hábito
-          </button>
-        </div>
+export function Header() {
+  return(
+    
+    <div className="w-full max-w-3xl mx-auto flex items-center justify-between">
+    <img src={logoImage} alt="Habits" />
+
+    <button
+     type="button"
+     className="border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300"
+     >
+      <Plus size={20} className="text-violet-500"/>
+      Novo hábito
+    </button>
+  </div>
+  )
+}
