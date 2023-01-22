35f1b45456ad7bb97beebac78a775ef125c54e8c
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2023-01-22 10:47:27 -0300

    Update ProgressBar.tsx

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -5,7 +5,7 @@ interface ProgressBarProps {
 export function ProgressBar(props: ProgressBarProps) {
   return (
     <div className='h-3 rounded-xl bg-zinc-700 w-full mt-4'>
-      <div 
+      <> 
         role="progressbar"
         aria-label="Progresso de hábitos completados nesse dia"
         aria-valuenow={props.progress}
@@ -13,7 +13,7 @@ export function ProgressBar(props: ProgressBarProps) {
         style={{
           width: `${props.progress}%`
         }}
-      />
+      </>
     </div>
   )
 }
